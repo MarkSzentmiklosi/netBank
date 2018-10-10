26e0e9b7dda3ec5c0f5c04a7e44f21fb07df50e1
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-10-10 19:40:28 +0200

    Repository interfaces of models

---
+++ src/main/java/banking/repository/BankAccountRepository.java
@@ -0,0 +1,9 @@
+package banking.repository;
+
+import banking.model.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount,Long> {
+}
