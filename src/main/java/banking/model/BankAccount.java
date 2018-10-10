8cae600cdd0d599d0957d8fcf7554c5f9cd66d88
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-10-10 17:14:03 +0200

    No need for generic prefix

--- src/main/java/banking/model/BankAccount.java
+++ src/main/java/banking/model/BankAccount.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankAccount<T> {
+public class BankAccount {
 
     private static long nextId = 1;
 
