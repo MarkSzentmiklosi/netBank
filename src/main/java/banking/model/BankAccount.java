50141d3b78ac37e6e5dcf6ae44b0fded6b6638e2
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-10-10 14:08:49 +0200

    Id field for identifying bank accounts

--- src/main/java/banking/model/BankAccount.java
+++ src/main/java/banking/model/BankAccount.java
@@ -6,10 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount<T> {
+
+    private static long nextId = 1;
+
+    long accountID;
     BigDecimal balance;
     List<Transaction> transactionHistory;
+
     {
         balance = BigDecimal.ZERO;
         transactionHistory = new ArrayList<Transaction>();
+        accountID = nextId++;
     }
+
 }
